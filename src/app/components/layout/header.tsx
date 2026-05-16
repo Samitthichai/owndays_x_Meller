fa5c8b976d8d343fed08e00bf720c6c5ab1b2018
Samitthichai Peeragun <samitthichai@odds.team>
2026-05-16 17:49:14 +0700

    [REFACTOR] Refactor section header and hero image by split file

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,39 +1,28 @@
 import logoImg from "@/assets/images/Collab-Logo.png";
-import heroImg from "@/assets/images/owndays_meller_header_bg.png";
+import { Text } from "@/components/ui/typography";
 import { navLinks } from "@/constant/navlink";
 import Image from "next/image";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   return (
-    <div className="sticky w-full h-158.25 top-0 z-50 max-w-360">
+    <header className="fixed top-0 left-0 z-50 flex items-center justify-between w-full py-5.75 lg:py-6.25 container-main">
       <Image
-        src={heroImg}
-        alt="OWNDAYS × MELLER BG"
-        fill
-        className="object-cover object-top "
+        src={logoImg}
+        alt="OWNDAYS x MELLER"
+        className="object-cover w-60 h-4 lg:w-87.75 lg:h-13"
         priority
       />
-
-      <nav className="absolute flex items-center justify-between py-6.25 container-main">
-        <Image
-          src={logoImg}
-          alt="OWNDAYS × MELLER"
-          className="object-cover w-87.75 h-13"
-          priority
-        />
-        <ul className="flex gap-12.5 ">
-          {navLinks.map((link) => (
-            <li key={link}>
-              <a href="#" className="nav-link">
-                {link}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="text-gt-american-display">PRODUCTS</div>
-      </div>
-    </div>
+      <ul className="hidden lg:flex gap-12.5">
+        {navLinks.map((link) => (
+          <li key={link}>
+            <Text variant="navLink" as="a" href="#">
+              {link}
+            </Text>
+          </li>
+        ))}
+      </ul>
+      <MobileMenu />
+    </header>
   );
 }
---
+++ src/app/components/layout/hero-section.tsx
@@ -0,0 +1,24 @@
+import heroDesktop from "@/assets/images/owndays_meller_header_bg.png";
+import heroMobile from "@/assets/images/owndays_meller_header_mobile.png";
+import { Text } from "@/components/ui/typography";
+
+export default function HeroSection() {
+  return (
+    <section className="relative w-full h-169.25 lg:h-158.25 -mt-15.5 ">
+      <picture className="block w-full h-full">
+        <source media="(min-width: 1024px)" srcSet={heroDesktop.src} />
+        <img
+          src={heroMobile.src}
+          alt="OWNDAYS x MELLER hero"
+          className="w-full h-full object-cover lg:object-top"
+        />
+      </picture>
+
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+        <Text variant="display" as="div">
+          PRODUCTS
+        </Text>
+      </div>
+    </section>
+  );
+}

526b10e5349157206e04673ae8e18b06642dba94
Samitthichai Peeragun <samitthichai@odds.team>
2026-05-17 13:05:14 +0700

    [UPDATE] Add utils format price amd product code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ProductSKU } from "@/types/product";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
+  return `¥${price.toLocaleString("ja-JP")}`;
+}
+
+export function formatProductCode(productCode: string, sku: ProductSKU) {
+  return `${productCode} ${sku.code}`;
 }

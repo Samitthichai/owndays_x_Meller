b4ee05a82f6ef703ef461347bec62592ea8f4839
Samitthichai Peeragun <samitthichai@odds.team>
2026-05-19 18:45:02 +0700

    [UPDATE]Move file format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -13,3 +13,17 @@ export function formatPrice(price: number): string {
 export function formatProductCode(productCode: string, sku: ProductSKU) {
   return `${productCode} ${sku.code}`;
 }
+
+export function buildImageUrl(path: string | null): string {
+  return path ? `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${path}` : "";
+}
+
+export function getColorName(sku: ProductSKU, lang = "en"): string {
+  return sku.colors
+    .map(
+      (c) =>
+        c.localizations.find((l) => l.language_code === lang)?.name ?? c.name,
+    )
+    .join(" / ")
+    .toUpperCase();
+}

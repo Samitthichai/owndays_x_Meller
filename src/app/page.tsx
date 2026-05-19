c505dbb6e44e2e2661a42a79797b51b49a4942da
Samitthichai Peeragun <samitthichai@odds.team>
2026-05-19 18:54:40 +0700

    [UPDATE] Plug  section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Header from "@/components/layout/header/header";
+"use client";
 import HeroSection from "@/components/sections/hero-section";
+import HowToStyleThem from "@/components/sections/how-to-style-them-section";
+import ProductSection from "@/components/sections/product-section";
 
 export default function Home() {
   return (
     <div className="relative">
-      <Header />
       <HeroSection />
+      <div className="mt-10 mb-37.5">
+        <ProductSection />
+      </div>
+      <HowToStyleThem />
     </div>
   );
 }

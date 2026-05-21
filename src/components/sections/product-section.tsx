27f930afda6cbf3b765dc8e94714c612ec0d59d6
Samitthichai Peeragun <samitthichai@odds.team>
2026-05-21 16:40:36 +0700

    [UPDATE]Enhance handling not found product

--- src/components/sections/product-section.tsx
+++ src/components/sections/product-section.tsx
@@ -1,8 +1,18 @@
 import ProductCard from "@/components/ui/product-card";
+import { Text } from "@/components/ui/typography";
 import { getProducts } from "@/services/product.service";
-
 export default async function ProductSection() {
-  const { data: products } = await getProducts();
+  const products = await getProducts();
+
+  if (products === null) {
+    return (
+      <div className="py-10 container-main">
+        <Text variant="gt" className="text-card-product-name-title text-center">
+          Can not get product.
+        </Text>
+      </div>
+    );
+  }
 
   return (
     <div className="py-10">
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,9 +1,13 @@
 import apiClient from "@/lib/axios";
-import type { ProductsResponse } from "@/types/product";
+import type { ProductItem, ProductsResponse } from "@/types/product";
 
-export async function getProducts(): Promise<ProductsResponse> {
-  const { data } = await apiClient.get<ProductsResponse>(
-    "/meller/products.json",
-  );
-  return data;
+export async function getProducts(): Promise<ProductItem[] | null> {
+  try {
+    const { data } = await apiClient.get<ProductsResponse>(
+      "/meller/products.json",
+    );
+    return data.data;
+  } catch {
+    return null;
+  }
 }
